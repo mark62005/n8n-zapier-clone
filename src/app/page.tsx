13efb09dcfd7fda0220d1f027f0a60f413647497
Mark Wong <markwong.digital@gmail.com>
2025-12-07 00:26:58 -0800

    Implement authentication check and display signed-in message on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
-export default function Home() {
-	return <div className="">Hello World</div>;
+import { requireAuth } from "@/lib/utils/auth";
+import { caller } from "@/trpc/server";
+
+export default async function Home() {
+	await requireAuth();
+
+	const authUser = await caller.auth.getAuthUser();
+
+	return (
+		<div className="min-h-screen min-w-screen flex flex-col items-center justify-center gap-y-6">
+			{authUser && <span>Signed in sucessfully.</span>}
+		</div>
+	);
 }
