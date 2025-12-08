bb4cc5f931179e92d9bd1863f4d9682767f851df
Mark Wong <markwong.digital@gmail.com>
2025-12-08 02:50:23 -0800

    Test ai provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import SignOutButton from "@/components/auth/SignOutButton";
 import { Button } from "@/components/ui/button";
 import { useTRPC } from "@/trpc/client";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { toast } from "sonner";
 
 export default function Home() {
 	const trpc = useTRPC();
@@ -22,9 +23,24 @@ export default function Home() {
 		})
 	);
 
+	const testAi = useMutation(
+		trpc.testings.testAi.mutationOptions({
+			onSuccess: () => {
+				toast.success("AI Job queued.");
+			},
+		})
+	);
+
 	return (
 		<div className="min-h-screen min-w-screen flex flex-col items-center justify-center gap-y-6">
 			<div>{JSON.stringify(workflows, null, 2)}</div>
+
+			<Button
+				onClick={() => testAi.mutate()}
+				disabled={testAi.isPending}
+			>
+				Test AI
+			</Button>
 
 			<Button
 				onClick={() => createWorkflow.mutate()}
