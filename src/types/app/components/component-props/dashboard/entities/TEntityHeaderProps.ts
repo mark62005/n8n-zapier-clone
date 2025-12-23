e7623d4d89d7f47b17e88b0b0551e06e233a4da8
Mark Wong <markwong.digital@gmail.com>
2025-12-22 23:34:17 -0800

    Build reusable EntityHeader component

---
+++ src/types/app/components/component-props/dashboard/entities/TEntityHeaderProps.ts
@@ -0,0 +1,11 @@
+export type TEntityHeaderProps = {
+	title: string;
+	description?: string;
+	newButtonLabel: string;
+	disabled?: boolean;
+	isCreating?: boolean;
+} & (
+	| { handleNewButtonClick: () => void; newButtonHref?: never }
+	| { newButtonHref: string; handleNewButtonClick?: never }
+	| { handleNewButtonClick?: never; newButtonHref?: never }
+);
